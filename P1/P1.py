b5296774305e0b58f8ce85817c95f1c4b5259384
Francisco Javier Jiménez Aznar <fjja0004@red.ujaen.es>
2025-02-08 16:31:57 +0100

    Correcciones menores

--- P1/P1.py
+++ P1/P1.py
@@ -1,5 +1,3 @@
-
-
 class Alumno:
     def __init__(self, nombre, apellido, dni, correo):
         self.__nombre = nombre
@@ -7,89 +5,84 @@ class Alumno:
         self.__dni = dni
         self.__correo = correo
 
-    def getnombre(self):
+    def get_nombre(self):
         return self.__nombre
-    def getapellido(self):
+
+    def get_apellido(self):
         return self.__apellido
-    def getdni(self):
+
+    def get_dni(self):
         return self.__dni
-    def getcorreo(self):
+
+    def get_correo(self):
         return self.__correo
 
     @staticmethod
-    def creaAlumno():
+    def crea_alumno(self):
         nombre = input("Nombre: ")
         apellido = input("Apellido: ")
         dni = input("DNI: ")
         correo = input("Correo: ")
         return Alumno(nombre, apellido, dni, correo)
 
-    def muestraAlumno(self):
+    def muestra_alumno(self):
         print("Datos del alumno:")
-        print("Nombre: " + self.getnombre())
-        print("Apellido: " + self.getapellido())
-        print("DNI: " + self.getdni())
-        print("Correo: " + self.getcorreo())
+        print("Nombre: " + self.get_nombre())
+        print("Apellido: " + self.get_apellido())
+        print("DNI: " + self.get_dni())
+        print("Correo: " + self.get_correo())
 
 
 class AlumnoIA(Alumno):
-    def __init__(self, nombre, apellido, dni, correo, grupoT, grupoP, listanotas = None):
+    def __init__(self, nombre, apellido, dni, correo, grupoT, grupoP, lista_notas=None):
         Alumno.__init__(self, nombre, apellido, dni, correo)
         self.__grupoT = grupoT
         self.__grupoP = grupoP
-        self.__listanotas = listanotas
+        self.__lista_notas = lista_notas
 
-    def getgrupoT(self):
+    def get_grupoT(self):
         return self.__grupoT
-    def getgrupoP(self):
-        return self.__grupoP
-    def getlistanotas(self):
-        return self.__listanotas
 
-    def nuevasNotas(self):
+    def get_grupoP(self):
+        return self.__grupoP
+
+    def get_lista_notas(self):
+        return self.__lista_notas
+
+    def nuevas_notas(self):
         notas = []
         notas.append(int(input("Nota de la practica 1: ")))
         notas.append(int(input("Nota de la practica 2: ")))
         notas.append(int(input("Nota de la practica 3: ")))
         notas.append(int(input("Nota de la practica 4: ")))
-        self.__listanotas = notas
+        self.__lista_notas = notas
 
-        suma=0
+        suma = 0
         for nota in notas:
-            suma=suma+nota
-        print("La media de la practica es: " + str(suma/4))
+            suma = suma + nota
+        print("La media de la practica es: " + str(suma / 4))
 
 
 def main():
-
-    nombrefichero = "datos1.txt"
+    nombre_fichero = "datos1.txt"
     modo = "rt"
-    alumnosimpares = []
+    alumnos_impares = []
 
-    fichero = open(nombrefichero, modo)
-    listafichero = fichero.readlines()
+    fichero = open(nombre_fichero, modo)
+    lista_fichero = fichero.readlines()
     fichero.close()
 
-    for linea in listafichero:
-        listalinea=linea.strip().split(",")
-        dni=int(listalinea[1])
-        if dni%2 == 1 : #El alumno tiene un dni impar
-            alumnosimpares.append(linea)
+    for linea in lista_fichero:
+        lista_linea = linea.strip().split(",")
+        dni = int(lista_linea[1])
+        if dni % 2 == 1:  # El alumno tiene un dni impar
+            alumnos_impares.append(linea)
 
-    nombrefichero = "impares.txt"
+    nombre_fichero = "impares.txt"
     modo = "wt"
-    fichero = open(nombrefichero, modo)
-    fichero.writelines(alumnosimpares)
+    fichero = open(nombre_fichero, modo)
+    fichero.writelines(alumnos_impares)
     fichero.close()
-
-
-
 
 
 main()
-
-
-
-
-
-
